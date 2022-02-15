8ca763a38d46a3861e31dee733cb3bbc95b926f2
Frazor Sharp <daocamberskies@googlemail.com>
2022-02-15 06:50:46 +0000

    ECS ideas for Scene ... Entity ... Components.

---
+++ Amberskies/Source/Amberskies/ECS/Scene.cpp
@@ -0,0 +1,57 @@
+
+#include "Scene.h"
+
+/**
+* ________________________________________________________
+* Project Created by Frazor Sharp : 14/02/2022
+*
+* Twitch : Simple Server Programming in C/C++
+*
+* Contact Email : daocamberskies@googlemail.com
+* Github		: https://github.com/Avengez/Amberskies.git
+* Twitch		: frazorbladezsharp
+* Youtube		: Frazor Sharp
+*
+* ______________________________________________________*/
+
+
+
+
+namespace Amber
+{
+
+	Scene::Scene()
+	{
+
+		/* ideas based on github entt
+		*  struct TransformComponent
+		* {
+		*		Data
+		* };
+		* 
+		*  u32 entity = m_Registry.create
+		* 
+		*  
+		*  m_Registry.emplace(targetEntity, Component)
+		*		.remove
+		*		.clear
+		*		.get
+		*		.group (componentType, componentType) use in same way as view
+		*		.connect to ie: OnComponentConstruct()
+		*	auto view = 	.view(componentType)
+		* 
+		*	for (auto entity : view)
+		*   {
+		*      do stuff
+		*   }
+		* 
+		*/
+	}
+	
+	
+	
+	Scene::~Scene()
+	{
+	}
+
+}
---
+++ Amberskies/Source/Amberskies/ECS/Scene.h
@@ -0,0 +1,40 @@
+#pragma once
+
+/**
+* ________________________________________________________
+* Project Created by Frazor Sharp : 15/02/2022
+*
+* Twitch : Simple Server Programming in C/C++
+*
+* Contact Email : daocamberskies@googlemail.com
+* Github		: https://github.com/Avengez/Amberskies.git
+* Twitch		: frazorbladezsharp
+* Youtube		: Frazor Sharp
+*
+* ______________________________________________________*/
+
+
+
+
+
+#include "Common.h"
+
+
+
+namespace Amber
+{
+
+	class Scene
+	{
+
+		// registry m_Registry
+
+	public:
+
+		Scene();
+
+		~Scene();
+
+	};
+
+}
